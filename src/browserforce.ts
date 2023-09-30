2be31edef58d2472df020691155638ccd3811888
buildbot <buildbot@adiza.dev>
2023-09-30 23:33:35 +1000

    fix: update puppeter to v21

--- src/browserforce.ts
+++ src/browserforce.ts
@@ -34,7 +34,7 @@ export class Browserforce {
         // workaround for navigating frames https://github.com/puppeteer/puppeteer/issues/5123
         '--disable-features=site-per-process'
       ],
-      headless: !(process.env.BROWSER_DEBUG === 'true')
+      headless: !(process.env.BROWSER_DEBUG === 'new')
     });
     await this.openPage(
       `secur/frontdoor.jsp?sid=${
